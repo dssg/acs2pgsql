d6f34ac75ea3ba75280454a27516f0f4bf917b5d
jmausolf <jgm346@gmail.com>
2016-06-29 21:28:54 +0000

    Updates to setup file.

--- acs_setup.sh
+++ acs_setup.sh
@@ -12,39 +12,44 @@
 
 state=Tennessee
 state_initial=tn
+city_geoid=US47037
+
 
 ##########################################################################
+## --------------------- USER DO NOT MODIFY BELOW --------------------- ##
+##########################################################################
 
-
-
-echo $state
-echo $state_initial
+echo "Selected State and Location:"
+echo $state, $state_initial, $city_geoid
 
 #Write Python Script
+#rm config.py || :
+touch config.py || rm config.py
 echo "#! /usr/bin/python" >> config.py
-echo "STATE = `echo $state`" >> config.py
-echo "STATE_INITIAL = `echo $state_initial`" >> config.py
+echo "STATE = '`echo $state`'" >> config.py
+echo "STATE_INITIAL = '`echo $state_initial`'" >> config.py
+echo "CITY_ID = '`echo $city_geoid`'" >> config.py
 
 #Write Shell Script
+#rm config.sh || :
+touch config.sh || rm config.sh
 echo "#!/bin/bash" >> config.sh
 echo "STATE = `echo $state`" >> config.sh
 echo "STATE_INITIAL = `echo $state_initial`" >> config.sh
 
-#
-
 
 #Make Scripts Executable
+echo "Making Scripts Executable..."
 chmod +x config.py
 chmod +x config.sh
-
+chmod +x input/acs/util.py
 chmod +x input/acs/import.py
 chmod +x input/acs/download.py
 chmod +x input/acs/census_postgres.sh
 
 
-
-
 #Setup Initial File Structure
-mkdir input && cd input && mkdir acs || :
-#cd input
-#mkdir acs || :
+#mkdir input && cd input && mkdir acs || :
+
+
+echo "Setup complete...you are now clear to Drake."
